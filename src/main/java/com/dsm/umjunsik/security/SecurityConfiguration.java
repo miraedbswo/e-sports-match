8528ad8e63d781cf0f44be8c34887b9f214805f6
miraedbswo <miraedbswo@gmail.com>
2020-07-24 09:42:01 +0900

    닉네임 중복 체크 permit-all로 권한 승격

--- src/main/java/com/dsm/umjunsik/security/SecurityConfiguration.java
+++ src/main/java/com/dsm/umjunsik/security/SecurityConfiguration.java
@@ -34,8 +34,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .antMatchers("/auth/signup").permitAll()
-                .antMatchers("/auth/login").permitAll()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/users/duplicate").permitAll()
                 .anyRequest().authenticated().and()
                 .apply(new JwtConfigurer(jwtTokenProvider)).and()
                 .apply(new ExceptionConfigurer());
